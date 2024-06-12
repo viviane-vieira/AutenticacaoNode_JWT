0fb0db5eaa7951f123a091ebb75595ad1f0de0ee
viviane-vieira <112434357+viviane-vieira@users.noreply.github.com>
2024-06-12 20:05:55 -0300

    feat: finalizando autenticação com o token

--- app.js
+++ app.js
@@ -20,7 +20,39 @@ const User = require('./models/User')
 //OPEN ROUTE - PUBLIC ROUTE
 app.get('/', (req,res) => {
     res.status(200).json({message:"bem vindo"})
+
 })
+//PRIVATE ROUTE
+app.get("/user/:id",checkToken, async (req,res) => {
+    const id = req.params.id
+
+    //CHECK IF USER EXISTS
+    const user = await User.findById(id, '-password')
+
+    if(!user){
+        return res.status(404).json({message: "Usuário não encontrado"})
+    }
+    res.status(200).json({user})
+})
+
+function checkToken(req,res,next){
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+    
+    if(!token) {
+        return res.status(401).json({message: "Acesso Negado!"})
+
+    }
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        next()
+        
+    } catch (error) {
+        res.status(400).json({message: "Token Inválido!"})
+    }
+}
 
 //REGISTER USE
 app.post('/auth/register', async(req,res) => {
@@ -69,6 +101,49 @@ app.post('/auth/register', async(req,res) => {
         
     }
 
+})
+//LOGIN USER
+app.post("/auth/login", async (req,res) =>{
+
+    const {email, password} =req.body
+
+    //validations
+    if(!email) {
+        if(!email){
+            return res.status(422).json({message: "O Email é obrigatorio!"})
+        }
+        if(!password){
+            return res.status(422).json({message: "A Senha é obrigatoria!"})
+        }
+    }
+    //CHECK IF USER EXISTS
+    const user = await User.findOne({email : email})
+
+    if(!user) {
+        return res.status(404).json({message: "Usuario não encontrado!"})
+    }
+    //CHECK IF PASSWORD MATCH
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+        return res.status(422).json({message: "Senha inválida!"})
+    }
+    try {
+        const secret = process.env.SECRET
+
+        const token = jwt.sign({
+            id: user._id
+        },
+        secret,
+      )
+
+      res.status(200).json({message: "Autentivação realizada com sucesso", token})        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message:"Aconteceu um erro no servidor, tente novamente mais tarde!"
+        })
+    }
 })
 
 
